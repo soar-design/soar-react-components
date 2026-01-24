3595f3258092a13fa3557c92ca96b482dfee7266
Mostapha Qammari <mostapha@alqammari.com>
2026-01-24 17:49:33 +0200

    Refactor InputGroup styles to update border radius and height for improved visual consistency; modify related CSS in TypeScript and JavaScript files

--- dist/index.js
+++ dist/index.js
@@ -30458,8 +30458,8 @@ function $9({ className: e, ...t }) {
       "data-slot": "input-group",
       role: "group",
       className: R(
-        "group/input-group border-input dark:bg-input/30 relative flex w-full items-center rounded-md border shadow-xs transition-[color,box-shadow] outline-none",
-        "h-9 min-w-0 has-[>textarea]:h-auto",
+        "group/input-group border-input dark:bg-input/30 relative flex w-full items-center rounded-xl border transition-[color,box-shadow] outline-none",
+        "h-12 min-w-0 has-[>textarea]:h-auto",
         // Variants based on alignment.
         "has-[>[data-align=inline-start]]:[&>input]:pl-2",
         "has-[>[data-align=inline-end]]:[&>input]:pr-2",
--- src/components/ui/input-group.tsx
+++ src/components/ui/input-group.tsx
@@ -12,8 +12,8 @@ function InputGroup({ className, ...props }: React.ComponentProps<"div">) {
       data-slot="input-group"
       role="group"
       className={cn(
-        "group/input-group border-input dark:bg-input/30 relative flex w-full items-center rounded-md border shadow-xs transition-[color,box-shadow] outline-none",
-        "h-9 min-w-0 has-[>textarea]:h-auto",
+        "group/input-group border-input dark:bg-input/30 relative flex w-full items-center rounded-xl border transition-[color,box-shadow] outline-none",
+        "h-12 min-w-0 has-[>textarea]:h-auto",
 
         // Variants based on alignment.
         "has-[>[data-align=inline-start]]:[&>input]:pl-2",
