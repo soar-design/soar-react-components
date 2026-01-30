987f6b721717bb389f2f3529e1595dcf03f911d8
Mostapha Qammari <mostapha@alqammari.com>
2026-01-31 01:22:04 +0200

    Update Tabs component styles to improve layout consistency by changing rounded corner classes from 'rounded-3xl' to 'rounded-2xl'; update related dist files accordingly.

--- dist/index.js
+++ dist/index.js
@@ -33847,7 +33847,7 @@ function Une({
   );
 }
 const MX = Yt(
-  "rounded-3xl p-1 group-data-[orientation=horizontal]/tabs:h-9 data-[variant=line]:rounded-none group/tabs-list text-muted-foreground inline-flex w-fit items-center justify-center group-data-[orientation=vertical]/tabs:h-fit group-data-[orientation=vertical]/tabs:flex-col",
+  "rounded-2xl p-1 group-data-[orientation=horizontal]/tabs:h-9 data-[variant=line]:rounded-none group/tabs-list text-muted-foreground inline-flex w-fit items-center justify-center group-data-[orientation=vertical]/tabs:h-fit group-data-[orientation=vertical]/tabs:flex-col",
   {
     variants: {
       variant: {
