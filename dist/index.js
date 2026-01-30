bc87539ad6b47c2ca15e71b02461b2d368592673
Mostapha Qammari <mostapha@alqammari.com>
2026-01-30 23:07:29 +0200

    Refactor 'today' element styles in Calendar and index.js components to remove unnecessary rounded corner class, enhancing layout consistency; update related dist files accordingly.

--- dist/index.js
+++ dist/index.js
@@ -8756,7 +8756,7 @@ function YJ({
         range_middle: T("rounded-none", l.range_middle),
         range_end: T("rounded-e-full bg-accent", l.range_end),
         today: T(
-          "bg-accent text-accent-foreground rounded-full data-[selected=true]:rounded-none [&:last-child[data-selected=true]_button]:rounded-e-full [&:first-child[data-selected=true]_button]:rounded-s-full",
+          "bg-accent text-accent-foreground [&:last-child[data-selected=true]_button]:rounded-e-full [&:first-child[data-selected=true]_button]:rounded-s-full",
           l.today
         ),
         outside: T(
