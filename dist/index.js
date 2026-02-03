e841454c1938beb47e541e71a281df8f769fdc44
Mostapha Qammari <mostapha@alqammari.com>
2026-02-03 15:50:44 +0200

    Refactor toggle and button component styles to enhance visual consistency by incorporating border styles and hover effects; synchronize updates in dist files accordingly.

--- dist/index.js
+++ dist/index.js
@@ -34057,12 +34057,12 @@ var zX = [
 Ev.displayName = qT;
 var BX = Ev;
 const GT = Yt(
-  "inline-flex items-center justify-center gap-2 rounded-2xl text-sm font-medium hover:bg-muted hover:text-muted-foreground disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-brand-accent data-[state=on]:border-brand-border data-[state=on]:text-accent-foreground [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 [&_svg]:shrink-0 focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] outline-none transition-[color] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive whitespace-nowrap",
+  "inline-flex items-center justify-center gap-2 rounded-2xl text-sm border font-medium data-[state=off]:hover:bg-muted data-[state=off]:hover:text-muted-foreground disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-brand-accent data-[state=on]:border-brand-border data-[state=on]:text-accent-foreground [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 [&_svg]:shrink-0 focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] outline-none transition-[color] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive whitespace-nowrap",
   {
     variants: {
       variant: {
-        default: "bg-transparent",
-        outline: "border border-input data-[state=on]:border-brand-border bg-transparent hover:bg-accent hover:text-accent-foreground"
+        default: "border-transparent bg-transparent",
+        outline: "border-input data-[state=on]:border-brand-border bg-transparent data-[state=off]:hover:bg-accent data-[state=off]:hover:text-accent-foreground"
       },
       size: {
         default: "h-9 px-2 min-w-9",
