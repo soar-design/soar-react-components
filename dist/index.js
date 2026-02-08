40f57669aa9de5e7301b4360dc8b959776242dd0
Mostapha Qammari <mostapha@alqammari.com>
2026-02-08 15:24:07 +0200

    Update button component styles by adjusting height and padding for improved usability; synchronize changes in dist files accordingly.

--- dist/index.js
+++ dist/index.js
@@ -5011,14 +5011,14 @@ const Ls = Yt(
         link: "text-primary underline-offset-4 hover:underline"
       },
       size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
-        xs: "h-6 gap-1 rounded-full px-2 text-xs has-[>svg]:px-1.5 [&_svg:not([class*='size-'])]:size-3",
-        sm: "h-8 rounded-full gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-10 rounded-full px-6 has-[>svg]:px-4",
+        default: "h-10 px-4 py-2 has-[>svg]:px-3",
+        xs: "h-8 gap-1 rounded-full px-2 text-xs has-[>svg]:px-1.5 [&_svg:not([class*='size-'])]:size-3",
+        sm: "h-9 rounded-full gap-1.5 px-3 has-[>svg]:px-2.5",
+        lg: "h-11 rounded-full px-6 has-[>svg]:px-4",
         icon: "size-9",
-        "icon-xs": "size-6 rounded-full [&_svg:not([class*='size-'])]:size-3",
-        "icon-sm": "size-8",
-        "icon-lg": "size-10"
+        "icon-xs": "size-8 rounded-full [&_svg:not([class*='size-'])]:size-3",
+        "icon-sm": "size-9",
+        "icon-lg": "size-11"
       }
     },
     defaultVariants: {
