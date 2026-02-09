19d79b388f81511d8502d3478997010c30408a8f
Mostapha Qammari <mostapha@alqammari.com>
2026-02-09 02:11:18 +0200

    Update button component styles to enhance visual consistency by adjusting padding values; synchronize changes in dist files accordingly.

--- dist/index.js
+++ dist/index.js
@@ -5011,9 +5011,9 @@ const Ls = Yt(
         link: "text-primary underline-offset-4 hover:underline"
       },
       size: {
-        default: "h-10 px-4 py-2 has-[>svg]:px-3",
-        xs: "h-8 gap-1 rounded-full px-2 text-xs has-[>svg]:px-1.5 [&_svg:not([class*='size-'])]:size-3",
-        sm: "h-9 rounded-full gap-1.5 px-3 has-[>svg]:px-2.5",
+        default: "h-10 px-5 py-2 has-[>svg]:px-4",
+        xs: "h-8 gap-1 rounded-full px-3 text-xs has-[>svg]:px-2 [&_svg:not([class*='size-'])]:size-3",
+        sm: "h-9 rounded-full gap-1.5 px-4 has-[>svg]:px-3",
         lg: "h-11 rounded-full px-6 has-[>svg]:px-4",
         icon: "size-10",
         "icon-xs": "size-8 rounded-full [&_svg:not([class*='size-'])]:size-3",
